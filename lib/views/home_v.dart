eb6bf2b117f38ebb6948f082ecd2207ad4ada281
Valentin <218587221+val277@users.noreply.github.com>
2025-11-22 15:35:16 +0100

    conflicts fix

--- lib/views/home_v.dart
+++ lib/views/home_v.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hubert_eat/viewmodels/home_vm.dart';
+import 'package:hubert_eat/widgets/restauCard.dart';
 import 'package:provider/provider.dart';
 
 class HomeV extends StatefulWidget {
@@ -20,34 +21,51 @@ class _HomeVState extends State<HomeV> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Column(children: [tagsWidget()]));
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          spacing: 20,
+          children: [
+            tagsWidget(),
+            Restaucard(title: "Crousty"),
+            Restaucard(title: "Subway"),
+            Restaucard(title: "Quick"),
+            Restaucard(title: "Mcdo"),
+            Restaucard(title: "Burger King"),
+          ],
+        ),
+      ),
+    );
   }
 
   Widget tagsWidget() {
-    return SizedBox(
-      height: 50,
-      child: Consumer<HomeVm>(
-        builder: (context, viewModel, child) {
-          List<String> tags = viewModel.getTags();
-          return viewModel.restaurants.isEmpty
-              ? const Center(child: Text("No tags available"))
-              : ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: tags.length,
-                  itemBuilder: (context, index) => Padding(
-                    padding: const EdgeInsets.all(2.0),
-                    child: ChoiceChip(
-                      label: Text(tags[index]),
-                      selected: _selectedTag == tags[index],
-                      onSelected: (bool selected) {
-                        setState(() {
-                          _selectedTag = selected ? tags[index] : "";
-                        });
-                      },
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        height: 50,
+        child: Consumer<HomeVm>(
+          builder: (context, viewModel, child) {
+            List<String> tags = viewModel.getTags();
+            return viewModel.restaurants.isEmpty
+                ? const Center(child: Text("No tags available"))
+                : ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: tags.length,
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: ChoiceChip(
+                        label: Text(tags[index]),
+                        selected: _selectedTag == tags[index],
+                        onSelected: (bool selected) {
+                          setState(() {
+                            _selectedTag = selected ? tags[index] : "";
+                          });
+                        },
+                      ),
                     ),
-                  ),
-                );
-        },
+                  );
+          },
+        ),
       ),
     );
   }
--- lib/widgets/restauCard.dart
+++ lib/widgets/restauCard.dart
@@ -13,27 +13,33 @@ class Restaucard extends StatelessWidget {
     final double frais = Random().nextDouble() * 5;
     final int time = Random().nextInt(21) + 30;
 
-    return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 130,
-            width: 250,
-            child: ClipRRect(
-              borderRadius: BorderRadiusGeometry.circular(15),
-              child: Image.network("https://placehold.co/90x70.png"),
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0),
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 130,
+              width: double.infinity,
+              child: ClipRRect(
+                borderRadius: BorderRadiusGeometry.circular(15),
+                child: Image.network(
+                  "https://placehold.co/600x400.png",
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-          ),
-          Text(
-            title,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-          ),
-          Text(
-            'frais de livraison à ${frais.toStringAsFixed(2)} € - $time min',
-          ),
-          Text('${avis.toStringAsFixed(1)}⭐ ($nombreAvis+ )'),
-        ],
+            Text(
+              title,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            Text(
+              'frais de livraison à ${frais.toStringAsFixed(2)} € - $time min',
+            ),
+            Text('${avis.toStringAsFixed(1)}⭐ ($nombreAvis+ )'),
+          ],
+        ),
       ),
     );
   }
