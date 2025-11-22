1d93d0f11f3c8d8befce2491da46b918d4bab8e3
Valentin <218587221+val277@users.noreply.github.com>
2025-11-22 15:17:48 +0100

    Tags widget and base of home view

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hubert_eat/viewmodels/home_vm.dart';
 import 'package:hubert_eat/views/main.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => HomeVm())],
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
